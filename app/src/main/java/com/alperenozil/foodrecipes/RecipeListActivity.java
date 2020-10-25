398db7fc9862ff5b1458901a35c188f0b6b427c0
necdetozil <necdet.ozil@hacettepe.edu.tr>
2020-10-25 11:39:57 +0300

    RecipeListViewModel is created.

--- app/src/main/java/com/alperenozil/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/alperenozil/foodrecipes/RecipeListActivity.java
@@ -1,6 +1,8 @@
 package com.alperenozil.foodrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +11,10 @@ import android.view.View;
 import com.alperenozil.foodrecipes.models.Recipe;
 import com.alperenozil.foodrecipes.requests.RecipeApi;
 import com.alperenozil.foodrecipes.requests.ServiceGenerator;
+import com.alperenozil.foodrecipes.requests.responses.RecipeResponse;
 import com.alperenozil.foodrecipes.requests.responses.RecipeSearchResponse;
 import com.alperenozil.foodrecipes.util.Constants;
+import com.alperenozil.foodrecipes.viewmodels.RecipeListViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,15 +25,27 @@ import retrofit2.Response;
 
 public class RecipeListActivity extends BaseActivity {
     public static final String TAG="RecipeListActivity";
+    private RecipeListViewModel recipeListViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+        recipeListViewModel= ViewModelProviders.of(this).get(RecipeListViewModel.class);
         testRetrofitRequest();
+        subscriveObservers();
+    }
+
+    private void subscriveObservers(){
+        recipeListViewModel.getRecipes().observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(List<Recipe> recipes) {
+
+            }
+        });
     }
 
     private void testRetrofitRequest(){
-        RecipeApi recipeApi= ServiceGenerator.getRecipeApi();
+        /*RecipeApi recipeApi= ServiceGenerator.getRecipeApi();
         Call<RecipeSearchResponse> responseCall=recipeApi
                 .searchRecipe(Constants.API_KEY,"chicken breast","1");
         responseCall.enqueue(new Callback<RecipeSearchResponse>() {
@@ -47,6 +63,25 @@ public class RecipeListActivity extends BaseActivity {
 
             @Override
             public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+
+            }
+        });*/
+        RecipeApi recipeApi= ServiceGenerator.getRecipeApi();
+        Call<RecipeResponse> responseCall=recipeApi
+                .getRecipe(Constants.API_KEY,"49421");
+        responseCall.enqueue(new Callback<RecipeResponse>() {
+            @Override
+            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+                Log.d(TAG, "onResponse: server response "+response.toString());
+                if (response.code()==200){
+                    Log.d(TAG, "onResponse: "+response.body().toString());
+                    Recipe recipe=response.body().getRecipe();
+                    Log.d(TAG, "onResponse: recipe "+recipe.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeResponse> call, Throwable t) {
 
             }
         });
